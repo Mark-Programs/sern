50ce98e7b322316b1b0e61a7fd509073c61bb8a6
Mark-Programs <mwwebphoto@gmail.com>
2022-04-25 11:41:11 -0500

    added fortunes initial data

--- firstServer/index.js
+++ firstServer/index.js
@@ -10,7 +10,15 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   const { url } = req; // set 'url' to req.url
   console.log(url); // shows everything typed after "localhost:3000"
-  res.end("Welcome to Node");
+
+  if (url === "/translations") {
+    const translations = { 1: "one", 2: "two", 3: "three" };
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(translations)); // pass a stringed version of the translations object
+    res.end();
+  } else {
+    res.end("Welcome to Node");
+  }
 });
 
 server.listen(port, hostname, () => {
